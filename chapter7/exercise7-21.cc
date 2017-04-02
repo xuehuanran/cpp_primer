de43ae489e6f6a0ed26f0e7b8035ae7c1fa68e4f
xuehuanran <xuehuanran@yahoo.com>
2017-04-02 14:46:10 +0800

    Access Control and Encapsulation

---
+++ chapter7/exercise7-21.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "exercise7-21.h"
+
+using std::cin;
+using std::istream;
+using std::cout;
+using std::endl;
+using std::cerr;
+
+int main() {
+    Sales_data total(cin);
+    if (!total.isbn().empty()) {
+        istream &is = cin;
+        while (is) {
+            Sales_data trans(is);
+            if (total.isbn() == trans.isbn()) {
+                total.combine(trans);
+            } else {
+                print(cout, total) << endl;
+                total = trans;
+            }
+        }
+        print(cout, total) << endl;
+    } else {
+        cerr << "No data?" << endl;
+        return -1;
+    }
+    return 0;
+}
